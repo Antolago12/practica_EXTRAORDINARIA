f4d813bc95c990ecad7d1411db25c90535f9f11f
Antonio Lago <anto.lago.1212@gmail.com>
2025-06-24 22:48:17 +0300

    añadido el back y front

---
+++ ALB/src/main/java/es/ufv/dis/back/examenFinal/ALB/AlbApplication.java
@@ -0,0 +1,13 @@
+package es.ufv.dis.back.examenFinal.ALB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbApplication.class, args);
+	}
+
+}
